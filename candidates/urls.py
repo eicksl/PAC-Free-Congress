bf9eef0a8d17e1e044d9fc3e04a3388ff96c2e88
Spencer Eick <spencereick@gmail.com>
2018-04-13 13:51:59 -0400

    Use ListView for results.html

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 
 app_name = 'candidates'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    #path('', views.IndexView.as_view(), name='index'),
+    path('', TemplateView.as_view(
+        template_name='candidates/index.html'), name='index'),
+    path('results', views.ResultsView.as_view(
+        template_name='candidates/results.html'), name='results')
 ]
--- candidates/views.py
+++ candidates/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.base import TemplateView
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
+#from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views import View
+from candidates.models import Candidate, PrimaryDate
+import requests
 
 
-class IndexView(TemplateView):
-    """The IndexView class provides functionality for the homepage view."""
+GOOGLE_API_URI = 'https://www.googleapis.com/civicinfo/v2/representatives'
+GOOGLE_KEY = open('google_key.txt').read()
 
-    template_name = 'candidates/index.html'
 
-    def index(request):
-        """Renders the homepage."""
-        return render(request, 'candidates/index.html')
+class ResultsView(ListView):
+    """Controls the results view."""
+
+    context_object_name = 'data'
+
+    def get_queryset(self):
+        abbr, district = self.addr_lookup()
+        return PrimaryDate.objects.all()
+
+
+    def addr_lookup(self):
+        params = {'key': GOOGLE_KEY, 'address': self.request.GET['address']}
+        resp = requests.get(GOOGLE_API_URI, params)
+        if resp.status_code != 200:
+            redirect('/')
+        #abbr = resp['normalizedInput']['state']
+        return None, None
+
+
+    def get_context_data(self, **kwargs):
+        """Override default to add primary dates to context."""
+        context = super().get_context_data(**kwargs)
+        context['datax'] = 'sup'
+        return context
+
+
+    def search(self):
+        template = 'candidates/results.html'
+        context = {'results': get_data(request.GET.get('address', ''))}
+        return render(request, template, context)
+
+
+    def get_data(self):
+        params = {'key': GOOGLE_KEY, 'address': address}
+        resp = requests.get(GOOGLE_API_URI, params).json()
