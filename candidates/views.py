1075f1c6039d8cc63e7fb45fb3b43f7b766aac1c
Spencer Eick <spencereick@gmail.com>
2018-04-14 12:00:39 -0400

    Create API request and SQL query logic for ResultsView

--- candidates/views.py
+++ candidates/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 #from django.http import HttpResponse
 #from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.contrib import messages
 from django.views import View
 from candidates.models import Candidate, PrimaryDate
 import requests
@@ -9,40 +9,113 @@ import requests
 
 GOOGLE_API_URI = 'https://www.googleapis.com/civicinfo/v2/representatives'
 GOOGLE_KEY = open('google_key.txt').read()
+AT_LARGE_STATES = set({'AK', 'DE', 'MT', 'ND', 'SD', 'VT', 'WY'})
 
 
-class ResultsView(ListView):
-    """Controls the results view."""
-
-    context_object_name = 'data'
-
-    def get_queryset(self):
-        abbr, district = self.addr_lookup()
-        return PrimaryDate.objects.all()
+class IndexView(View):
+    """Homepage view."""
+    def get(self, request):
+        """Renders the homepage."""
+        template = 'candidates/index.html'
+        return render(request, template)
 
 
-    def addr_lookup(self):
-        params = {'key': GOOGLE_KEY, 'address': self.request.GET['address']}
-        resp = requests.get(GOOGLE_API_URI, params)
-        if resp.status_code != 200:
-            redirect('/')
-        #abbr = resp['normalizedInput']['state']
-        return None, None
-
-
-    def get_context_data(self, **kwargs):
-        """Override default to add primary dates to context."""
-        context = super().get_context_data(**kwargs)
-        context['datax'] = 'sup'
-        return context
-
-
-    def search(self):
+class ResultsView(View):
+    """Handles Google Civic Info API requests and SQL querying for the results
+    page."""
+    def get(self, request):
         template = 'candidates/results.html'
-        context = {'results': get_data(request.GET.get('address', ''))}
+        address = request.GET.get('address', None)
+        if address is None:
+            messages.add_message(request, messages.INFO,
+                'Please enter a state or an address')
+            return redirect('candidates:index')
+        try:
+            abbr, district = self.addr_lookup(address)
+        except AssertionError:
+            messages.add_message(request, messages.INFO,
+                'The state or address was not recognized')
+            return redirect('candidates:index')
+        if district is None:
+            context = {
+                'state_scope': True,
+                'results': self.state_scope(abbr)
+            }
+        else:
+            context = {
+                'state_scope': False,
+                'results': self.district_scope(abbr, district)
+            }
+        context.update({'primary_date': self.find_primary_date(abbr)})
         return render(request, template, context)
 
 
-    def get_data(self):
-        params = {'key': GOOGLE_KEY, 'address': address}
-        resp = requests.get(GOOGLE_API_URI, params).json()
+    def state_scope(self, abbr):
+        if abbr in AT_LARGE_STATES:
+            return self.district_scope(abbr, '0')
+        house = Candidate.objects.filter(abbr=abbr, office__startswith='House')
+        districts_dict = {}
+        for candidate in house:
+            district = int(candidate.office.split('-')[1])
+            if not district in districts_dict:
+                districts_dict[district] = []
+            districts_dict[district].append(candidate)
+        return {
+            'senate': self.find_senate(abbr),
+            'house': sorted(districts_dict.items())
+        }
+
+
+    def district_scope(self, abbr, district):
+        house_office = 'House-' + district
+        house = Candidate.objects.filter(abbr=abbr, office=house_office)
+        return {
+            'senate': self.find_senate(abbr),
+            'house': house
+        }
+
+
+    def find_senate(self, abbr):
+        """Retrieves senate candidates for the given state abbreviation."""
+        return Candidate.objects.filter(
+            abbr=abbr, office__startswith='Senate')
+
+
+    def find_primary_date(self, abbr):
+        """Retrieves the primary date for the given state abbreviation."""
+        return PrimaryDate.objects.get(abbr=abbr).date
+
+
+    def addr_lookup(self, address):
+        params = {'key': GOOGLE_KEY,'address': address}
+        resp = requests.get(GOOGLE_API_URI, params)
+        assert resp.status_code == 200
+        resp_json = resp.json()
+        abbr = resp_json['normalizedInput']['state']
+        if abbr in AT_LARGE_STATES:
+            return abbr, '0'
+
+
+
+        try:
+            val = list(resp_json['divisions'].keys())[2].rsplit(':', 1)[1]
+            int(val)
+            district = val
+        except (IndexError, ValueError):
+            district = None
+
+
+
+        """
+        if resp_json['normalizedInput']['zip']:
+            for key in resp_json['divisions'].keys():
+                val = key.rsplit(':', 1)[1]
+                try:
+                    int(val)
+                except ValueError:
+                    continue
+                district = val
+        else:
+            district = None
+        """
+        return abbr, district
